81984d3037b3bf450fc29010def9590ab48a86fd
sharu-35 <sharuit123@gmail.com>
2024-07-23 11:50:51 +0530

    final commend

--- Advice/src/Component/Advice.jsx
+++ Advice/src/Component/Advice.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import "./Advice.css"
 const Advice = () => {
-  return (
+    const [advice,setadvice]= useState("Please click button to get advice")
+  const[count,setcount]=useState(0);
+ async function getadvice(){
+    const res = await fetch("https://api.adviceslip.com/advice")
+    const data = await res.json();
+    setadvice(data.slip.advice);
+    setcount((c)=>c+1)
+ }
+  
+  
+  
+    return (
     <div>
-    
+    <h3>{advice}</h3>
+    <button onClick={getadvice}> Get Advice</button>
+    <p>You have read <b>{count}</b> pieces of advice</p>
     </div>
   )
 }
